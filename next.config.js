b942e6c8389c1a855beebe17600ad37c5fa0dcde
Marian Zburlea <marianzburlea@gmail.com>
2021-06-22 15:21:45 +0100

    one more try

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination:
+          'https://do-not-drink-beer-marianzburlea.vercel.app/api/:path*',
+      },
+    ]
+  },
 }
