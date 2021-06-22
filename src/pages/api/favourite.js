5a060348b7388e94c4491f0cfd820ad20e242215
Marian Zburlea <marianzburlea@gmail.com>
2021-06-22 15:33:38 +0100

    smart messages

--- src/pages/api/favourite.js
+++ src/pages/api/favourite.js
@@ -51,15 +51,21 @@ const handler = async (request, response) => {
       const testRef = firestore.collection('test')
       // get list
       const getSnap = await testRef.doc(userId).get()
-      const data = getSnap.data().list || []
-      const newData = data.includes(beerId)
-        ? data.filter((id) => id !== beerId)
-        : [...data, beerId]
+      if (getSnap) {
+        const data = getSnap.data().list || []
+        const newData = data.includes(beerId)
+          ? data.filter((id) => id !== beerId)
+          : [...data, beerId]
 
-      testRef.doc(userId).set({ list: newData })
-      response.status(200).json({
-        message: 'I am on it boss!',
-      })
+        testRef.doc(userId).set({ list: newData })
+        response.status(200).json({
+          message: 'My favourite job!',
+        })
+      } else {
+        response.status(200).json({
+          message: 'Nothing happened',
+        })
+      }
     } catch (error) {
       response.status(500).json({
         message: 'error',
